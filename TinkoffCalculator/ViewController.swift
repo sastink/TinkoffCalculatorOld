c0c12586e12e4044e6b0dfa22ba69a7978389998
sastink <sshlyahin@gmail.com>
2024-01-23 09:11:11 +0300

    fix

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-        guard let buttonText = sender.currentTitle	else { return}
-        print(buttonText)}
+        guard let buttonText = sender.currentTitle	else { return }
+        print(buttonText)
+    }
 }
